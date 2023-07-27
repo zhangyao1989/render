cdb2551de939705f27b142661dc0f70174c0eced
zhangyao1989 <120291265+zhangyao1989@users.noreply.github.com>
2023-07-28 00:58:36 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '03b45cc5-b8c0-4e28-9c06-e5b56509ad43';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
